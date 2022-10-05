811e0386673b9df7c9749eced628e4199c0c37f5
alanmoraales <alanmoraales845@gmail.com>
2022-10-05 13:52:59 -0500

    add initial setup with chakra ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import themes from "./themes";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ColorModeScript
+      initialColorMode={themes.defaultTheme.config.initialColorMode}
+    />
+    <ChakraProvider theme={themes.defaultTheme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
